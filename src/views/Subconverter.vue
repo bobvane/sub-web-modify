1ead3e4a394336c6dd2e19108c80b2cef1203aa5
bobvane <58875973+bobvane@users.noreply.github.com>
2025-09-05 15:28:14 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -499,7 +499,7 @@ export default {
             options: [
               {
                 label: "默认（Bob-crypto）",
-                value: "https://raw.githubusercontent.com/bobvane/clash/refs/heads/main/Clash-mini.ini"
+                value: "https://raw.githubusercontent.com/bobvane/clash/refs/heads/main/Clash-mini-bob.ini"
               },
               {
                 label: "默认（Bob-mini）",
@@ -877,7 +877,7 @@ export default {
         clientType: "",
         customBackend: this.getUrlParam() == "" ? "https://subapi.bobvane.top" : this.getUrlParam(),
         shortType: "https://v1.mk/short",
-        remoteConfig: "https://raw.githubusercontent.com/bobvane/clash/refs/heads/main/Clash-mini.ini",
+        remoteConfig: "https://raw.githubusercontent.com/bobvane/clash/refs/heads/main/Clash-mini-bob.ini",
         excludeRemarks: "",
         includeRemarks: "",
         filename: "",
@@ -1399,6 +1399,7 @@ export default {
   }
 };
 </script>
+
 
 
 
